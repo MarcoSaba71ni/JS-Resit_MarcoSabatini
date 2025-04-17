7abd05c08ed13d271c6a92e6d29b0e0bbb7aa848
Marco Sabatini <marsab04188@stud.noroff.no>
2025-04-17 12:00:40 +0200

    changes

--- js/check-out.js
+++ js/check-out.js
@@ -1,34 +1,42 @@
- // Call this function when the page loads
+document.addEventListener("DOMContentLoaded", () => {
+  displayTotal();
+});
+
 function displayTotal() {
-    const totalCheckout = document.getElementById('total-checkout');
+  const totalCheckout = document.getElementById("total-checkout");
+  const totalSpan = document.getElementById("total-span");
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
   
-    const cart = JSON.parse(localStorage.getItem('cart')) || [];
-  
-    cart.forEach(game => {
-      generateTotal(game);
-    });
-  }
-  
-  // Generate and render each game's summary info
-  function generateTotal(game) {
-    const totalDiv = document.createElement('div');
-  
-    const totalImg = document.createElement('img');
-    totalImg.src = game.image.url;
-    totalImg.alt = game.image.alt;
-    totalImg.className = "image-total";
-  
-    const totalHeading = document.createElement('h4');
-    totalHeading.textContent = game.title;
-  
-    const totalSingPrice = document.createElement('p');
-    totalSingPrice.textContent = `Price: $${game.price}`;
-  
-    totalDiv.appendChild(totalImg);
-    totalDiv.appendChild(totalHeading);
-    totalDiv.appendChild(totalSingPrice);
-  
-    const totalCheckout = document.getElementById('total-checkout');
-    totalCheckout.appendChild(totalDiv);
-  }
-  
+  let totalPrice = 0;
+
+  cart.forEach(game => {
+    generateTotal(game);
+    totalPrice += game.price;
+  });
+
+  totalSpan.textContent =  `TOTAL PRICE: $${totalPrice.toFixed(2)}`;
+}
+
+function generateTotal(game) {
+  const totalDiv = document.createElement("div");
+
+  const totalImg = document.createElement("img");
+  totalImg.src = game.image.url;
+  totalImg.alt = game.image.alt;
+  totalImg.className = "image-total";
+
+  const totalHeading = document.createElement("h4");
+  totalHeading.textContent = game.title;
+
+  const totalSingPrice = document.createElement("p");
+  totalSingPrice.textContent = `Price: $${game.price.toFixed(2)}`;
+
+  totalDiv.appendChild(totalImg);
+  totalDiv.appendChild(totalHeading);
+  totalDiv.appendChild(totalSingPrice);
+
+  const totalCheckout = document.getElementById("total-checkout");
+  totalCheckout.appendChild(totalDiv);
+}
+
+
